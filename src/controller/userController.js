878c6a19d7e53ba7cfa510b4cbf5981e3bebb5d0
Hp <yahye2092@gmail.com>
2023-12-03 06:38:40 +0300

    first commit

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,6 +3,7 @@ import User from "../models/user.js";
 
 
 export const register = async(req,res)=>{
+  try {
     const {name,email,avator}= req.body;
     const userFind = await User.findOne({email});
     if(userFind){
@@ -11,9 +12,14 @@ export const register = async(req,res)=>{
     const user = new User({name,email,avator});
     await user.save();
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({msg:error});
+    
+  }
 }
 
 export const getUser=async(req,res)=>{
+  try {
     const {id} = req.params;
     const user = await User.findById(id);
     
@@ -21,5 +27,18 @@ export const getUser=async(req,res)=>{
         return res.status(404).json({msg:"user not found"});
     }
     res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({msg:error});
+    
+  }
+}
+export const getAllUsers=async(req,res)=>{
+  try {
+    const user = await User.find();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({msg:error});
+    
+  }
 }
 
--- src/router/user_router.js
+++ src/router/user_router.js
@@ -1,8 +1,9 @@
 import express  from 'express';
-import { getUser, register } from '../controller/userController.js';
+import { getAllUsers, getUser, register } from '../controller/userController.js';
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/:id").get(getUser);
+router.route("/").get(getAllUsers);
 export default router;
